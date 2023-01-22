1f97cc02f452cad94c41dbda0d660997ac96be95
MarryAngel <maria.angel.kmiranda@gmail.com>
2023-01-21 22:00:17 -0300

    Base do Labirinto + ProBuilder

---
+++ Assets/Scripts/MovimentoCaixa.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCaixa : MonoBehaviour
+{
+    
+    //Controla a velocidade de rotação da caixa
+    public float velocidade;
+    
+    //Método executa após o cálculo de física
+    private void FixedUpdate() {
+        
+        float desl = velocidade * Time.fixedDeltaTime, 
+              rotacaoHorizontal = Input.GetAxis("Horizontal") * desl,
+              rotacaoVetical = Input.GetAxis("Vertical") * desl;
+
+        transform.Rotate(rotacaoVetical, 0, rotacaoHorizontal);
+
+    }
+
+}
